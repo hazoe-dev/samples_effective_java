2b09be8d501acc11b999d5ba9ca477aef51da9a7
hazoe-dev <hatran.dev@gmail.com>
2023-04-26 07:25:30 +0700

    apply for field declare in equal lombok

--- samples_effective_java/src/common_method/lombok/Book.java
+++ samples_effective_java/src/common_method/lombok/Book.java
@@ -1,13 +1,15 @@
 package common_method.lombok;
 
 import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode
-public class Book {
+//luon chi dinh ro cac nao @EqualsAndHashCode.Include
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Book extends Product{
     @EqualsAndHashCode.Exclude private int publishingYear;
     @EqualsAndHashCode.Include static int count;//static -> lombok exclude
     //because static -> class memory: all variables have one value.
-    public Book(int publishingYear) {
+    transient int transientVar = 10;
+    public Book(int publishingYear, int quantity) {
+        super(quantity);
         this.publishingYear = publishingYear;
         count++;
     }
@@ -21,9 +23,18 @@ public class Book {
     }
 
     public static void main(String[] args) {
-        Book b1 = new Book(2012);
-        Book b2 = new Book(2013);
+        Book b1 = new Book(2012,2);
+        Book b2 = new Book(2013,8);
         boolean result = b1.equals(b2);//true
+        System.out.println(result);
     }
 
+}
+
+class Product{
+    int quantity;
+
+    public Product(int quantity) {
+        this.quantity = quantity;
+    }
 }
