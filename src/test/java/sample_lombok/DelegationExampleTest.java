df85dfdb1f27e9c9f9906cbc413e5ba726b8de8f
hazoe-dev <hatran.dev@gmail.com>
2023-05-28 23:35:35 +0700

    Example for delegate lombok

--- src/test/java/sample_lombok/DelegationExampleTest.java
+++ src/test/java/sample_lombok/DelegationExampleTest.java
@@ -1,7 +1,32 @@
 package sample_lombok;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DelegationExampleTest {
+    private DelegationExample delegationExample;
+    @Test
+    public void add_whenEmptyCollection_returnTrue(){
+        //arrange
+        delegationExample = new DelegationExample();
+        //act
+        boolean actual = delegationExample.add("Hi");
+        //assert
+        Assertions.assertEquals(true, actual);
+        Assertions.assertEquals(1, delegationExample.getCollection().size());
+    }
 
+    @Test
+    public void remove_whenCollectionHasOneItem_returnTrue(){
+        //arrange
+        delegationExample = new DelegationExample();
+        delegationExample.add("Hi");
+        //act
+        boolean actual = delegationExample.remove("Hi");
+        //assert
+        Assertions.assertEquals(true, actual);
+        Assertions.assertEquals(0, delegationExample.getCollection().size());
+    }
 }
