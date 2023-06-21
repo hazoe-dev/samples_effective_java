548ce4d3024a47d59da498f28b58f67a36929ec2
hazoe-dev <hatran.dev@gmail.com>
2023-06-21 07:59:52 +0700

    Chain of responsibility.

--- src/main/java/chain_of_resposiblity/MyChain.java
+++ src/main/java/chain_of_resposiblity/MyChain.java
@@ -9,29 +9,50 @@ import java.util.List;
 public class MyChain {
     private List<String> filters = Arrays.asList(new String[]{FilterA.class.getName(),FilterB.class.getName()});
     private MyFilter instance;
+    private MyFilter next;
 
-    public MyChain(MyFilter instance) {
+    public MyChain(MyFilter instance, MyFilter next) {
         this.instance = instance;
+        this.next = next;
     }
 
     public void doFilter(String request, String response) throws Exception{
-        if(instance==null){
+        if(instance==null && next == null){
             MyFilter filter = createFilter(filters.get(0));
             MyFilter nextFilter = createFilter(filters.get(1));
 
-            filter.doFilter(request,response,new MyChain(nextFilter));
+            filter.doFilter(request,response,new MyChain(filter, nextFilter));
+        }else if (instance!=null && next != null){
+            next.doFilter(request,response,new MyChain(next, getNextFilter(next)));
         }else{
-
-            int index = filters.indexOf(instance.getClass().getName());
-            if(filters.size()==index-1){
-
-            }else{
-                MyFilter nextFilter = createFilter(filters.get(index++));
-                instance.doFilter(request,response, new MyChain(nextFilter));
-            }
-
+            return;
         }
     }
+
+    private MyFilter getNextFilter(MyFilter current) {
+        //get next index!
+        String  filterName =  filters.stream().filter(item -> item.equals(current.getClass().getName()))
+                .findAny()
+                .orElse("");
+        if(filterName.isEmpty()){
+            return null;
+        }else{
+            try {
+                return createFilter(filterName);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     private MyFilter createFilter(String classname) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> c = Class.forName(classname);
         Constructor<?> cons = c.getConstructor();
--- src/main/java/chain_of_resposiblity/MyServlet.java
+++ src/main/java/chain_of_resposiblity/MyServlet.java
@@ -4,8 +4,9 @@ public class MyServlet{
     MyChain chain;
 
     protected void doGet(String req, String resp) throws Exception {
-        chain = new MyChain(null);
+        chain = new MyChain(null, null);
         chain.doFilter(req,resp);
+
         System.out.println("Servlet");
 
     }
