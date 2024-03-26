07e2201d79e5692f1c2096586071a12e0da98074
hazoe-dev <hatran.dev@gmail.com>
2024-03-26 08:13:00 +0700

    test with chap 67

--- src/main/java/thread_concurrentMap/mylock/Main.java
+++ src/main/java/thread_concurrentMap/mylock/Main.java
@@ -1,6 +1,9 @@
 package thread_concurrentMap.mylock;
 
 import java.util.HashSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,12 +15,37 @@ public class Main {
 //            }
 //        });
 
+//        set.addObserver(new SetObserver<Integer>() {
+//            public void added(ObservableSet<Integer> s, Integer e) {
+//                System.out.println(e);
+//                if (e == 23) s.removeObserver(this);
+//            }
+//        });
+
         set.addObserver(new SetObserver<Integer>() {
-            public void added(ObservableSet<Integer> s, Integer e) {
+            public void added(final ObservableSet<Integer> s, Integer e) {
                 System.out.println(e);
-                if (e == 23) s.removeObserver(this);
+                if (e == 23) {
+                    ExecutorService executor =
+                            Executors.newSingleThreadExecutor();
+                    final SetObserver<Integer> observer = this;
+                    try {
+                        executor.submit(new Runnable() {
+                            public void run() {
+                                s.removeObserver(observer);
+                            }
+                        }).get();
+                    } catch (ExecutionException ex) {
+                        throw new AssertionError(ex.getCause());
+                    } catch (InterruptedException ex) {
+                        throw new AssertionError(ex.getCause());
+                    } finally {
+                        executor.shutdown();
+                    }
+                }
             }
         });
+
         for (int i = 0; i < 100; i++)
             set.add(i);
     }
